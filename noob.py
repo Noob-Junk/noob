fc2ab81d2c9a878a551de018e79cbb4f3d67f79a
Noob-Junk <77061416+Noob-Junk@users.noreply.github.com>
2021-01-08 20:10:12 +0800

    Update noob.py

--- noob.py
+++ noob.py
@@ -11,18 +11,19 @@ c = '\033[1;36;40m'
 os.system("clear")
 
 # From adrress & To address
-fromaddress = raw_input("\033[1;97mFrom address: ")
-toaddress = raw_input("\033[1;97mTo address: ")
+fromaddress = raw_input("\033[1;93mFrom address:\033[1;97m ")
+toaddress = raw_input("\033[1;93mTo address:\033[1;97m ")
 
 os.system("clear")
 print  """
-\033[1;97mTHIS IS FOR EDUCATIONAL PURPOSES ONLY
+\033[1;97mTHIS IS FOR EDUCATIONAL PURPOSES ONLY"""
+
 # Gmail Login
-username = raw_input("\033[1;93mYour email: ")
-password = getpass.getpass("\033[1;93mPassword: ")
+username = raw_input("\033[1;93mYour email:\033[1;97m ")
+password = getpass.getpass("\033[1;93mPassword:\033[1;97m ")
 
 # message
-message = raw_input("\033[1;97mYour message: ")
+message = raw_input("\033[1;93mYour message:\033[1;97m ")
 
 # Creating a connection
 server =smtplib.SMTP('smtp.gmail.com:587')
@@ -31,9 +32,9 @@ server.login(username,password)
 
 # for loop to send multiple spam
 total = input("Total spam: ")
-print c+"\n[*]"+w+" Running . . ."+w
+print c+"\n[*]"+w+" Sending . . ."+w
 for i in range (total):
   server.sendmail(fromaddress,toaddress,message)
-  print g+"[*]"+w+" mail sent"
+  print g+"[~]"+w+" mail sent"
   
 server.quit()
