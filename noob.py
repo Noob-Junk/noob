43a25bf18c85d8f652b1dfde7f383aaed9e28778
Noob-Junk <77061416+Noob-Junk@users.noreply.github.com>
2021-01-08 19:52:31 +0800

    Update noob.py

--- noob.py
+++ noob.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 import smtplib
 import getpass
 
@@ -7,16 +8,21 @@ w = '\033[0m'
 g = '\033[32m'
 c = '\033[1;36;40m'
 
-# From adrress & To address
-fromaddress = raw_input("From address: ")
-toaddress = raw_input("To address: ")
+os.system("clear")
 
+# From adrress & To address
+fromaddress = raw_input("\033[1;97mFrom address: ")
+toaddress = raw_input("\033[1;97mTo address: ")
+
+os.system("clear")
+print  """
+\033[1;97mTHIS IS FOR EDUCATIONAL PURPOSES ONLY
 # Gmail Login
-username = raw_input("Your email: ")
-password = getpass.getpass("Password: ")
+username = raw_input("\033[1;93mYour email: ")
+password = getpass.getpass("\033[1;93mPassword: ")
 
 # message
-message = raw_input("Your message: ")
+message = raw_input("\033[1;97mYour message: ")
 
 # Creating a connection
 server =smtplib.SMTP('smtp.gmail.com:587')
@@ -28,6 +34,6 @@ total = input("Total spam: ")
 print c+"\n[*]"+w+" Running . . ."+w
 for i in range (total):
   server.sendmail(fromaddress,toaddress,message)
-  print g+"[*]"+w+" mail sent !"
+  print g+"[*]"+w+" mail sent"
   
 server.quit()
